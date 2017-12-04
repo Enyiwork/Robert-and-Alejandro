73ef77638ee66bec25e2edd5ae29033d8f7d0546
Enyiwork <32691424+Enyiwork@users.noreply.github.com>
2017-12-03 21:25:50 -0500

    add style and finish interaction 2

---
+++ src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import contactList from '../contacts';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+  contacts: Object[];
+  newContact: Object = {};
+
+  constructor() { }
+
+  ngOnInit() {
+    this.contacts = contactList;
+  }
+
+  addContact(){
+    console.log("Add contact has been called");
+    // add contact to contacts list
+    // clear inputs
+    this.contacts.push(this.newContact);
+
+    // clear the input
+    for(let reset in this.newContact) {
+      this.newContact[reset] = "";
+    }
+  }
+}
